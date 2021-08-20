161fe1c7df8bef55e0357003f5a23a3243b6fbd4
Anush-007 <82879558+Anush-007@users.noreply.github.com>
2021-08-21 00:35:39 +0530

    iimplemented turn method

--- snake.py
+++ snake.py
@@ -1,19 +1,23 @@
 from .vector import Vector
-from enum import Enum, auto
+from enum import Enum
+from random import choice
 
 class Direction(Enum):
-	left = auto()
-	right = auto()
-	up = auto()
-	down = auto()
+	left = Vector(-1, 0)
+	right = Vector(1, 0)
+	up = Vector(0, -1)
+	down = Vector(0, 1)
 
 class Snake():
-	def __init__(self, start, body = []) -> None:
+	def __init__(self, start, body = [], speed = 10) -> None:
 		self.head = Vector(*start)
 		self.body = body
+		self.speed = speed
+		self.veldir = choice(list(Direction))
 
 	def eat():
 		pass
 
-	def turn():
-		pass
+	def turn(self, direction : Direction):
+		if self.veldir != -direction:
+			self.veldir = direction
